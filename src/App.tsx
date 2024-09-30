9694c9728cca5feca0103ac833f5d2e47d186722
LYK315 <ben_lyk@live.com>
2024-09-30 22:55:03 +0100

    dsa

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import About from './components/About/About';
 import Journey from './components/Journey/Journey';
 import Skills from './components/Skills/Skills';
 import Projects from './components/PrevProject/Projects';
-import Connect from './components/Connect/Connect';
+import Connect from './components/ConnectMe/Connect';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
