afe82c6def283d1719747b81945c6d1df6ea9fd3
LYK315 <yikai.liaw13@gmail.com>
2025-02-19 13:39:12 +0000

    Test alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
+// import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@constants": path.resolve(__dirname, "./src/constants"),
-      "@contexts": path.resolve(__dirname, "./src/contexts"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@assets": path.resolve(__dirname, "./src/assets"),
+  //     "@components": path.resolve(__dirname, "./src/components"),
+  //     "@constants": path.resolve(__dirname, "./src/constants"),
+  //     "@contexts": path.resolve(__dirname, "./src/contexts"),
+  //     "@hooks": path.resolve(__dirname, "./src/hooks"),
+  //     "@stores": path.resolve(__dirname, "./src/stores"),
+  //     "@styles": path.resolve(__dirname, "./src/styles"),
+  //     "@utils": path.resolve(__dirname, "./src/utils"),
+  //   },
+  // },
 });
